3eec2b2b1bfbf220fadccc306fffafa30ab03a1c
Mihrac199 <mihrac.comart1996@gmail.com>
2025-04-06 03:21:56 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -37,13 +37,37 @@ function Logo() {
 
 function Form() {
 
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(e);
+    };
+
     return (
 
-        <div className="add-form">
+        <form className="add-form" onSubmit={handleSubmit}>
 
             <h3>What do you need for your trip?</h3>
 
-        </div>
+            <select>
+
+                {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
+
+                    <option value={num} key={num}>
+
+                        {num}
+
+                    </option>
+
+                ))}
+
+            </select>
+
+            <input type="text" placeholder="item..." />
+
+            <button type="submit">Add</button>
+
+        </form>
 
     );
 
